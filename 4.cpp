1332a081e01adafe56b3fdf5c3920a5331536e28
samarajit mane <samarjeetmane01@gmail.com>
2024-11-14 00:15:15 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    // while loop :
+    int n,i;
+    cout << "enter value of n :";
+    cin >> n;
+    i = 1; // this is declaration of starting or initial element;
+    while (i <=n)  // this is the codition 
+    {
+        cout << i << endl;  // the command we want to execute until condition is satisfied;
+        i = i +1; // the step we want to take after  excution 
+    }
+    return 0;
+
+
+} 

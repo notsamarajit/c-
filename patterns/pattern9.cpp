2893dac13eb6ca3bf34bfbd614c336b0cee9687d
samarajit mane <samarjeetmane01@gmail.com>
2024-11-27 22:11:58 +0530

    Add files via upload

---
+++ patterns/pattern9.cpp
@@ -0,0 +1,28 @@
+// 1
+// 2 3
+// 3 4 5
+// 4 5 6 7
+// print this pattern
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            cout << i + j - 1 << " ";
+            j = j + 1;
+
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    
+}
